7b0e48cb573e18f258ffef0637beba866ce22af4
abay94 <ae.aubakirov@gmail.com>
2023-02-20 20:44:35 +0600

    init

---
+++ src/main/java/com/example/demo_seq/DemoSeqApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_seq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSeqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSeqApplication.class, args);
+    }
+
+}
